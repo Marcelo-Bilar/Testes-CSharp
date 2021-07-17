fcd5a0a61e879ff61f07194c1e13cb95928479ef
Marcelo Bilar Rodrigues <marcelo.bilar2@gmail.com>
2021-07-17 14:35:49 -0300

    Orientação ao Objeto, Enums e Polimorfismo

---
+++ ListaDeAnimais/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ListaDeAnimais.Classes;
+namespace ListaDeAnimais
+{
+    /*
+        Escrever um programa  - OK
+        Que recebe um nome de animal vertebrada - OK 
+        De acordo com um filtro, identifica a qual dos grupos ele pertence (Mamifero, Réptil, Ave e Peixe) - OK 
+        E exibir os dados genéricos e exclusivos de cada grupo animal - OK
+    */
+    class Program
+    {
+        //Escrever um programa  - OK
+        static void Main(string[] argumentos)
+        {
+            // Que recebe um nome de animal vertebrada - OK 
+            Console.WriteLine("Favor informar o nome do animal: ");
+            var NomeAnimal = Console.ReadLine();
+            if(string.IsNullOrEmpty(NomeAnimal) || string.IsNullOrWhiteSpace(NomeAnimal))
+            {
+                Console.WriteLine("Favor passar um nome de animal vertebrado válido!");
+                return;
+            }
+            // Recebe uma lista de animais
+            var ListaDeAnimais = new List<Animal>();
+            ListaDeAnimais.Add(new Mamifero() {Nome= "Vaca", QuantidadeDeMamas = 4});
+            ListaDeAnimais.Add(new Reptil() {Nome= "Cobra"});
+            ListaDeAnimais.Add(new Ave() {Nome= "Gavião"});
+            ListaDeAnimais.Add(new Peixe() {Nome= "Tubarão", QuantidadeNadadeiras =2});
+
+            // Compara se o animal digitado é igual o da lista
+            var animalSelecionado = ListaDeAnimais.FirstOrDefault(animal => animal.Nome.ToUpper() == NomeAnimal.ToUpper());
+
+            // Se não for nenhum da lista
+            if(animalSelecionado == null)
+            {
+                Console.WriteLine("O animal não está na nossa lista!");
+                return;
+            }
+
+            //Se o animal digitado for um desses identificado mostra suas especificações
+            if(animalSelecionado is Mamifero)
+            {
+                Console.WriteLine($"O animal encontrado é um mamífero com nome {animalSelecionado.Nome} e quantidade de mamas = {((Mamifero)animalSelecionado).QuantidadeDeMamas}");
+            } else if(animalSelecionado is Reptil)
+            {
+                Console.WriteLine($"O animal encontrado é um reptil com nome {animalSelecionado.Nome} e não sabe controlar sua temperatura  = {((Reptil)animalSelecionado).ControlaTemperaturaDoCorpo}");
+            } else if(animalSelecionado is Ave)
+            {
+                Console.WriteLine($"O animal encontrado é uma ave com nome {animalSelecionado.Nome} e tem penas = {((Ave)animalSelecionado).TemPena}");
+            } else if(animalSelecionado is Peixe)
+            {
+                Console.WriteLine($"O animal encontrado é um peixe com nome {animalSelecionado.Nome} e tem = {((Peixe)animalSelecionado).QuantidadeNadadeiras} nadadeiras");
+            } else 
+            {
+                Console.WriteLine($"O animal encontrado é um animal com nome {animalSelecionado.Nome}");
+            }
+        }
+    }
+}
--- OrientacaoObjeto/Program.cs
+++ OrientacaoObjeto/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ListaProdutosMercadoClasse.Classes;
 
-namespace OrientacaoObjeto
+namespace ListaProdutosMercadoClasse
 {
     class Program
     {
         static void Main(string[] argumentos)
         {
-            Console.WriteLine("Hello World!");
+            List<Produto> produtosDisponiveis = new List<Produto>();
+            produtosDisponiveis.Add(new Produto() {Nome ="Pão", Preco=0.5});
+            produtosDisponiveis.Add(new Produto() {Nome ="Shampoo", Preco=19.90});
+            produtosDisponiveis.Add(new Produto() {Nome ="Azeite", Preco=14.5});
+            produtosDisponiveis.Add(new Produto() {Nome ="Refrigerante", Preco=6.25});
+            produtosDisponiveis.Add(new Produto() {Nome ="Queijo", Preco=15.75});
+            produtosDisponiveis.Add(new Produto() {Nome ="Carne", Preco=62.80});
+
+            if(argumentos.Length == 0)
+            {
+                Console.WriteLine("Você não passou os produtos que deseja comprar");
+                return;
+            }
+
+            var produtosSelecionadosDisponiveis = produtosDisponiveis.Where(produto => argumentos.Any(argumento => produto.Nome.ToUpper() == argumento.ToUpper()));
+            
+            foreach(var produto in produtosSelecionadosDisponiveis)
+            {
+                Console.WriteLine($"Este produto nós temos: {produto.ExibirDadosProdutos()}");
+            }
+
+            var produtosSelecionadosNaoDisponiveis = argumentos.Where(argumento => !produtosDisponiveis.Any(produto => produto.Nome.ToUpper() == argumento.ToUpper()));
+
+            foreach(var produtoNaoDisponivel in produtosSelecionadosNaoDisponiveis)
+            {
+                Console.WriteLine($"Este produto nós não temos infelizmente =/ : {produtoNaoDisponivel}");
+            }
+
+            var produtosOrdenadosPorNome = produtosDisponiveis.OrderBy(produto => produto.Nome).ToList();
+
+            foreach( var produtoOrdenado in produtosOrdenadosPorNome)
+            {
+                Console.WriteLine(produtoOrdenado.ExibirDadosProdutos());
+            }
         }
     }
 }
