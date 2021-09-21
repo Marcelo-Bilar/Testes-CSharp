2fd44121c6da6e27932480a4633718b287bc5f32
Marcelo Bilar Rodrigues <marcelo.bilar2@gmail.com>
2021-09-21 05:32:46 -0300

    Exercícios novos

---
+++ Exercícios de C#/Calculadora/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static float Realizaroperacao(float numero1, float numero2, string operacao)
+        {
+            switch(operacao)
+            {
+                case "+": return numero1 + numero2;
+                case "-": return numero1 - numero2;
+                case "*": return numero1 * numero2;
+                case "/": return numero1 / numero2;
+                case "%": return numero1 % numero2;
+                default: return 0f;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro número: ");
+            var numero1string = Console.ReadLine();
+            float numero1;
+            bool teste1 = float.TryParse(numero1string, out numero1);
+            if (!teste1)
+            {
+            Console.WriteLine("O primeiro número digitado não é valido.");
+            return;
+            }
+
+            Console.WriteLine("Digite o segundo número: ");
+            var numero2string = Console.ReadLine();
+            float numero2;
+            bool teste2 = float.TryParse(numero2string, out numero2);
+            if (!teste2)
+            {
+            Console.WriteLine("O segundo número digitado não é valido.");
+            return;
+            }
+            
+            Console.WriteLine("Digite a operação que deseja fazer: ");
+            var operacao = Console.ReadLine();
+
+            float resultado = Realizaroperacao(numero1, numero2, operacao);
+            Console.WriteLine("O resultado da sua operação é igual a " + resultado);
+
+            }
+    }
+}
---
+++ Exercícios de C#/TesteNumeroInteiro/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace programa2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Digite um número válido. ");
+                return;
+            }
+            int numero;
+            bool teste = int.TryParse(args[0], out numero);
+            if (teste == false)
+            {
+                Console.WriteLine("O número digitado não é um número inteiro ");
+            }
+
+            Console.WriteLine("O número digitado foi " + numero);
+        }
+    }
+}
